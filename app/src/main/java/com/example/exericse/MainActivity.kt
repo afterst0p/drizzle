142b4a4e7b2de5e777c2aa5761d4c9d681af5039
kalteon <102862962+kalteon@users.noreply.github.com>
2023-06-02 13:28:57 +0900

    feat: foreground TimeService

--- app/src/main/java/com/example/exericse/MainActivity.kt
+++ app/src/main/java/com/example/exericse/MainActivity.kt
@@ -2,17 +2,20 @@ package com.example.exericse
 
 import Client
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         //val cookieManager = (applicationContext as Cookie).cookieManager
-        val cookie = Cookie()
-        val client = Client(cookie)
+        //val cookie = Cookie()
+        //val client = Client(cookie)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,12 +32,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         //val cookieManager = (applicationContext as Cookie).cookieManager
         val cookie = Cookie()
         val client = Client(cookie)
 
+        //val timeWorkRequest = OneTimeWorkRequestBuilder<Worker>().build()
+        val timeService = Intent(this, TimeService::class.java)
         super.onResume()
+        /*
         client.loginCheck({ result ->
             println(result)
             if (!result.equals("not login")) {
@@ -45,6 +52,10 @@ class MainActivity : AppCompatActivity() {
                 println("로그인 실패")
             }
         }, this)
+        */
+        //WorkManager.getInstance(this).enqueue(timeWorkRequest)
+        Log.d("TimeService", "startForegroundService")
+        startForegroundService(timeService)
     }
 }
 
---
+++ app/src/main/java/com/example/exericse/TimeService.kt
@@ -0,0 +1,71 @@
+package com.example.exericse
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.exericse.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+class TimeService : Service() {
+
+    private val CHANNEL_ID = "TimeServiceChannel"
+    private val NOTIFICATION_ID = 1
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            updateTime()
+            handler.postDelayed(this, 1000)
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("TimeService", "onCreate")
+        createNotificationChannel()
+        notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Time Service")
+            .setSmallIcon(R.drawable.ic_notification)
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        handler.post(runnable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Time Service Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    private fun updateTime() {
+        val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+        val time = dateFormat.format(Date())
+        notificationBuilder.setContentText(time)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+}
